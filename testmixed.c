5152de7c6972309e13414bccc1efa801defe65f3
Ayushi-tomar02 <ayushitomar624@gmail.com>
2026-04-05 11:20:00 +0530

    Add files via upload

---
+++ testmixed.c
@@ -0,0 +1,22 @@
+int a = ;
+float b = 10.7.6;
+char a = 'a;
+string s = "Ayushi;
+
+int c = 12.4;
+
+a = b + ;          // syntax error
+
+if (a > ) {        // syntax error
+    a = 5;
+}
+int int y;
+2abc = 7;          // lexical error
+
+int a;             // semantic error (redeclaration)
+
+x = y + 3;         // semantic error
+
+12..5              // lexical error
+
+@                  // lexical error
