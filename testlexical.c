2f48cab98b79250b92f5a1a1a2fafaf58e6cd8ea
Ayushi-tomar02 <ayushitomar624@gmail.com>
2026-04-05 12:22:00 +0530

    Add files via upload

---
+++ testlexical.c
@@ -0,0 +1,9 @@
+int a = 10;
+float b = 5.5;
+char c = 'x';
+
+2abc = 5;          // invalid identifier
+a = 12.6.3;         // malformed number
+"hello  
+'a          // unterminated string
+@                 // invalid character
